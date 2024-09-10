d09fbb3c6c1b54c59c62b6b0dd26e200b05dc82a
DylanPJackson <frprdjackson@gmail.com>
2024-09-10 16:51:14 -0700

    Updated blog_list page with django template, added minor styling for convenience, updated blog_list view with new context

--- prof_site_app/views.py
+++ prof_site_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import HomePage, NavBar, Company
+from .models import HomePage, NavBar, Company, PursuitType, Pursuit, BlogPost
 
 
 def home(request):
@@ -22,15 +22,21 @@ def experience(request):
 
 def blog_list(request):
     nav_bar = NavBar.objects.get(pk=1)
+    pursuit_types = PursuitType.objects.all()
+    pursuits = Pursuit.objects.all()
+    blog_posts = BlogPost.objects.all()
     context = {
-        "nav_bar": nav_bar
+        "nav_bar": nav_bar,
+        "pursuit_types": pursuit_types,
+        "pursuits": pursuits,
+        "blog_posts": blog_posts
     }
     return render(request, "prof_site_app/blog_list.html", context)
 
 
 def placeholder(request, page_name: str):
     page_name = page_name.capitalize()
-    if page_name == "Projects" or page_name == "Blog" or page_name == "Passions":
+    if page_name == "Projects" or page_name == "Passions":
         context = {
             "image_link": 'prof_site_app/images/{}.jpg'.format(page_name.lower()),
             "page_name": page_name
